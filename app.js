c04455fc1f8ae33a99de2683d6fcf600bc137ce7
Vinicius Souza <vinicius.souza@nre.tec.br>
2022-12-22 20:08:03 -0300

    connect with db

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const cors = require("cors");
 const app = express();
 
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.listen(3000,()=>{
+//db connection
+
+const conn = require("./db/conn");
+
+conn();
+
+app.listen(3000, () => {
     console.log("server on")
 })
+
